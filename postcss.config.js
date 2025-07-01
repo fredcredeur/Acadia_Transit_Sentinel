f1f1691e5b9751e269c910e119cdc11ee10413d6
fredcredeur <38992196+fredcredeur@users.noreply.github.com>
2025-07-01 14:45:24 -0500

    Resolve Git merge conflicts in configuration and source files

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,4 @@ export default {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 3c24d9e62665244f95ff965ed5fc261ce073a64a
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
@@ -12,15 +11,3 @@ createRoot(document.getElementById('root')!).render(
     </ToastProvider>
   </StrictMode>
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 3c24d9e62665244f95ff965ed5fc261ce073a64a
