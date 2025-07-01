22222284d1b92ddd2f6691335a72fdbf211e6f52
fredcredeur <38992196+fredcredeur@users.noreply.github.com>
2025-07-01 14:50:19 -0500

    Resolve Git merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { Shield, Truck, Map, AlertTriangle } from 'lucide-react';
 import { VehicleForm } from './components/VehicleForm';
@@ -637,52 +636,8 @@ function App() {
           </div>
         </div>
       </main>
-=======
-import { useState } from 'react';
-import { MapComponent } from './components/MapComponent';
-import { RouteSelector } from './components/RouteSelector';
-import { LocationSearch } from './components/LocationSearch';
-import { RouteDetails } from './components/RouteDetails';
-import { Header } from './components/Header';
-import { Sidebar } from './components/Sidebar';
-import { MapProvider, useMapContext } from './contexts/MapContext';
-
-function AppContent() {
-  const { selectedRoute, setSelectedRoute } = useMapContext();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
-  return (
-    <div className="flex h-screen flex-col">
-      <Header toggleSidebar={toggleSidebar} />
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar isOpen={isSidebarOpen}>
-          <div className="space-y-4 p-4">
-            <LocationSearch />
-            <RouteSelector onRouteSelect={setSelectedRoute} />
-            {selectedRoute && <RouteDetails route={selectedRoute} />}
-          </div>
-        </Sidebar>
-        <main className="flex-1 relative">
-          <MapComponent />
-        </main>
-      </div>
->>>>>>> 3c24d9e62665244f95ff965ed5fc261ce073a64a
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default function App() {
-  return (
-    <MapProvider>
-      <AppContent />
-    </MapProvider>
-  );
-}
->>>>>>> 3c24d9e62665244f95ff965ed5fc261ce073a64a
